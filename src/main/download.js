a004d114f729df8710f55e46206abf7f766f75ca
devanshu <devanshu@localhost.localdomain>
2020-08-19 01:37:23 +0530

    changes issue fixed with merge and download

--- src/main/download.js
+++ src/main/download.js
@@ -1,15 +1,20 @@
 import {ipcMain} from 'electron';
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const glob=require('glob');
-//const fluent_ffmpeg = require("fluent-ffmpeg");
+const ffmpeg = require('ffmpeg-static');
 const path = require('path');
 const exec = require('child_process').exec;
+var vidList = [];
+if(require('os').platform() === 'win32'){
+    const pathToDownloads = `${process.env.HOME}\\Downloads`;
+    var pathToAll = `${pathToDownloads}\\YTvideo2\\`;
+    var pathToMerged = `${pathToDownloads}\\YTvideoMerged2/`;
+} else {
+    const pathToDownloads = `${process.env.HOME}/Downloads`;
+    var pathToAll = `${pathToDownloads}/YTvideo1/`;
+    var pathToMerged = `${pathToDownloads}/YTvideoMerged1/`;
 
-
-const pathToDownloads = process.platform === 'win32'?`${process.env.HOME}\\Downloads`:`${process.env.HOME}/Downloads`;
-const pathToAll = process.platform === 'win32'?`${pathToDownloads}\\YTvideo`:`${pathToDownloads}/YTvideo`;;
-const pathToMerged = process.platform === 'win32'?`${pathToDownloads}\\YTvideoMerged`:`${pathToDownloads}/YTvideoMerged`;
+}
 
 if(!fs.existsSync('intermediate')){
     fs.mkdirSync('intermediate')
@@ -18,13 +23,13 @@ if(!fs.existsSync('intermediate')){
 async function mergeVideos(inputVideos,pathToMerged,e){
     var ffmpeger = "";
     ffmpeger = inputVideos.map(video => 
-    `ffmpeg -y -i ${video} -c copy -bsf:v h264_mp4toannexb -f mpegts intermediate/${path.parse(video).name}.ts`).join("&&");
+    `${ffmpeg} -y -i ${video} -c copy -bsf:v h264_mp4toannexb -f mpegts intermediate/${path.parse(video).name}.ts `).join("&&");
     var concatenator = '"concat:';
     concatenator += inputVideos.map(video =>`intermediate/${path.parse(video).name}.ts`).join("|");
     concatenator+='"';
     var outputName = inputVideos.map(video=> path.parse(video).name).join("+")
-    var mergecmd = `ffmpeg -y -i ${concatenator} -c copy -bsf:a aac_adtstoasc ${pathToMerged}\\${outputName}.mp4`;
-    exec(`${ffmpeger}&&${mergecmd}`,(err, stdout, stderr) => {
+    var mergecmd = `${ffmpeg} -y -i ${concatenator} -c copy -bsf:a aac_adtstoasc ${pathToMerged}${outputName}.mp4`;
+    exec(`${ffmpeger} && ${mergecmd}`,(err, stdout, stderr) => {
         if(err) {
             console.log(err);
             e.sender.send('Error','Error while merging')
@@ -36,25 +41,32 @@ async function mergeVideos(inputVideos,pathToMerged,e){
 
 
 function downloadAll(e,pathToAll,urlList){
-    let task = []
     let videoId = []
+    var videoList = []
+    var videoTitle = []
+    var downloadComplete = 0
+    var vidNo = 0
     urlList.forEach(url => {
-        task.push(ytdl(url,{ format:'mp4' }))
-        videoId.push(path.parse(url).name.split('=')[1])
+        videoList[url] = ytdl(url);
+        var path = pathToAll + `abc${vidNo++}.mp4`;
+        vidList.push(path);
+        videoList[url].pipe(fs.createWriteStream(path));
+        videoList[url].on('progress', (chunkLength, downloaded, total) => {
+            const percent = downloaded / total;
+            console.log('downloading', `${(percent * 100).toFixed(1)}%`);
+        });
+        videoList[url].on('end', () => {
+            downloadComplete++;
+            if(downloadComplete == urlList.length){
+                e.sender.send('download-complete',`Downloaded ${urlList.length} videos successfuly`)
+            }
+        });
     })
-    Promise.all(task)
-        .then(res=>{
-            res.forEach((item,index)=>{
-               item.pipe(fs.createWriteStream(`${pathToAll}\\${videoId[index]}.mp4`))
-            })
-            e.sender.send('download-complete',`Downloaded ${urlList.length} videos successfuly`)
-        })
-        .catch((err)=>{
-            console.log(err) 
-        })
+        
 }
 
 ipcMain.on('download-all',(e,urlList)=>{
+    console.log("download");
     if(!fs.existsSync(pathToAll)){
         fs.mkdirSync(pathToAll)
     }
@@ -63,13 +75,9 @@ ipcMain.on('download-all',(e,urlList)=>{
 })
 
 ipcMain.on('merge',(e,arg)=>{
+    console.log("merge");
     if(!fs.existsSync(pathToMerged)){
         fs.mkdirSync(pathToMerged)
     }
-    const inputVideos = glob.sync(`${pathToAll}\\*.mp4`)
-    mergeVideos(inputVideos,pathToMerged,e);
+    mergeVideos(vidList,pathToMerged,e);
 })
-
-/*
-https://www.youtube.com/watch?v=ZgMw__KdjiI,https://www.youtube.com/watch?v=d-UU_lyqcFg,https://www.youtube.com/watch?v=Uw5JOtvFd-k
-*/ 
